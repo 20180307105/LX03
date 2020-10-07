13f627d8042370d62ee9e89131b79e5240f8f6c9
20180307105 <71053467+20180307105@users.noreply.github.com>
2020-10-07 18:48:49 +0800

    Add files via upload

---
+++ UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+   
+    public class UsersController : Controller
+    {
+        [HttpPost("page")]
+
+        public ActionResult getPage([FromBody]Model.Page page)
+        {
+            var result = DAL.UserInfo.Instance.GetPage(page);
+            if (result.Count() == 0)
+                return Json(Result.Err("返回记录数为0"));
+            else
+                return Json(Result.Ok(result));
+        }
+        
+    }
+}
